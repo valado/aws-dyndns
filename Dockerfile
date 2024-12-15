9eda57a32fa536d0d39e764bfbd5a20c2c2ef6fe
Vladimir Daskalov <vld.daskalov@gmail.com>
2024-12-15 02:24:54 +0100

    fix: parse interval

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY requirements.txt ./
 RUN python3 -m venv .venv
 RUN . .venv/bin/activate
 RUN pip install -r requirements.txt
+RUN apt update
+RUN apt -y install dnsutils
 
 COPY . .
 
--- common/consts.py
+++ common/consts.py
@@ -5,7 +5,7 @@ DOMAIN = os.environ.get('DOMAIN')
 IP_URL = 'https://ipinfo.io/ip'
 IP_TOKEN = os.environ.get('IP_TOKEN')
 TIMEOUT = 5
-CHECK_INTERVAL = os.environ.get('CHECK_INTERVAL')
+CHECK_INTERVAL = int(os.environ.get('CHECK_INTERVAL'))
 LOG_FILE_PATH = Path.home() / './ddns.log'
 AWS_CONFIG = {
     'aws_credentials': {
